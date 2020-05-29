d94200eedbe866313e91d89fcfc02707350cf5ad
Fernando Lima <fernando.fortlima@gmail.com>
2020-05-29 16:16:13 -0300

    criação de chamadas rest para o domínio das Receitas

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,16 +1,6 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const recipe = require("./public/Recipe");
 admin.initializeApp();
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.getUsers = functions.https.onRequest(async (request, response) => {
-  const listUsers = await admin
-    .firestore()
-    .collection("users")
-    .get()
-    .then((res) => {
-      return res.docs.map((x) => x.data());
-    });
-  response.send(listUsers);
-});
+
+exports.recipe = functions.https.onRequest(recipe.recipe);
